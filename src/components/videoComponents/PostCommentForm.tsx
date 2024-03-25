dbeaf45e799f8410f40e7bd4e88c1f73ae88c764
Juan Arede <85207414+juanarede@users.noreply.github.com>
2024-03-25 18:05:44 -0300

    wip

--- src/components/videoComponents/PostCommentForm.tsx
+++ src/components/videoComponents/PostCommentForm.tsx
@@ -4,7 +4,7 @@ export const PostCommentForm = () => {
 <div id="commentsContainer" className="w-full md:w-1/2 p-2 pt-4 rounded shadow-lg mx-auto sm:ml-4">
   <div className="flex">
     <div>
-      <h2 className="font-semibold"><i className="fa-regular fa-comment mr-2 ml-3"></i> Comment <span className="font-bold">19</span> </h2>
+      <h2 className="font-semibold"><i className="fa-regular fa-comment mr-2 ml-3"></i> Comments <span className="font-bold">19</span> </h2>
     </div>
   </div>
   <div className="mt-3 p-3">
